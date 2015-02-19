f127dfcd89cec651d8e678087aa95d6288840be2
Britt Gresham <britt@puppetlabs.com>
2015-02-18 23:38:13 -0800

    Continuing work on web bits

--- modules/brittbot/web.py
+++ modules/brittbot/web.py
@@ -1,15 +1,41 @@
 import threading
-from flask import Flask
+
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit, send
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+gjenni = None
 
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test_message():
+    gjenni.msg(
+        "##brittslittlesliceofheaven",
+        "This is a web posted message"
+    )
+    return str(gjenni.brain)
+
+
+@socketio.on('my event')
+def ws_logger(jenni, msg):
+    send({
+        'room': str(msg.sender),
+        'msg': str(msg),
+        'nick': str(msg.nick),
+    }, json=True, broadcast=True)
+ws_logger.rule = r".*"
 
 
 def setup(jenni):
-    jenni.flask = threading.Thread(target=app.run, kwargs={
+    global gjenni
+    gjenni = jenni
+    jenni.flask = threading.Thread(target=socketio.run, args=(app, ), kwargs={
         'host': '0.0.0.0',
     })
     jenni.flask.start()
