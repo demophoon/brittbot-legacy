6ce7cd51ea6c9c7e45702c32ad3d0e9f5bd6e17b
Britt Gresham <brittcgresham@gmail.com>
2015-06-24 21:35:20 +0000

    Obfuscate for the lawls

--- modules/brittbot/youtube.py
+++ modules/brittbot/youtube.py
@@ -33,14 +33,32 @@ def get_video_information(videoid, api_key):
 
 
 @smart_ignore
-def yt_context(jenni, input):
-    target = input.groups()[0]
-    print target
+def yt_context(jenni, msg):
+    import random
+    target = msg.groups()[0]
     video_id = parse_qs(urlparse(target).query).get("v")
+    obfuscate = parse_qs(urlparse(target).query).get("o")
+    if "QrGrOK8oZG8" in video_id and msg.nick == "demophoon":
+        obfuscate = True
+    if not obfuscate is None:
+        video_id = [random.choice([
+            # Maru
+            "bXtHwvp7jYE",
+            "8uDuls5TyNE",
+            "jgxL-PwmY7s",
+            "euCG3jbefH8",
+            "6lC2lbeY_rU",
+            "VKQtwvKa-aY",
+            "AeJX7fxz-f4",
+            # Music videos
+            #"gy1B3agGNxw",
+        ])]
     if not video_id:
         video_id = [urlparse(target).path[1:]]
     yt = get_video_information(video_id[0], jenni.config.youtube_api_key)
     if yt:
+        if not obfuscate is None:
+            yt['shorturl'] = "http://youtu.be/" + parse_qs(urlparse(target).query).get("v")[0]
         youtube = colorize("You", fg=colors['black'], bg=colors['white'])
         youtube += colorize("Tube", fg=colors['white'], bg=colors['red'])
         jenni.say("%s \"%s\" (%s) by %s - %s" % (
