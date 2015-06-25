27bcc323724e7042b47e5382e6dc673f086129bc
Britt Gresham <brittcgresham@gmail.com>
2015-06-25 15:15:27 +0000

    Add more bang functions

--- modules/brittbot/bang.py
+++ modules/brittbot/bang.py
@@ -9,10 +9,17 @@ from modules.brittbot.helpers import action
 
 
 @smart_ignore
+def mgs_alert(jenni, input):
+    jenni.say("http://brittg.com/zMJpt")
+mgs_alert.rule = r'^!+$'
+mgs_alert.priority = 'high'
+
+
+@smart_ignore
 def sadtuba(jenni, input):
     # add ?autoplay=true at the end of the link for button free "whaa whaa"
     jenni.say("http://sadtrombone.com")
-sadtuba.rule = "^(!|\x01ACTION )(sadtuba|sadtrombone)"
+sadtuba.rule = "^(!|\x01ACTION )sad(tuba|trombone|\w+)"
 sadtuba.priority = 'medium'
 
 
@@ -21,6 +28,12 @@ def rimshot(jenni, input):
     jenni.say("Buh dum tsh")
 rimshot.rule = "^(!|\x01ACTION )rimshot"
 rimshot.priority = 'medium'
+
+
+@smart_ignore
+def contributing(jenni, input):
+    jenni.say("https://github.com/demophoon/brittbot")
+contributing.rule = "^!contributing"
 
 
 @smart_ignore
@@ -42,10 +55,56 @@ tooedgy.rule = "^!(2|too)?edgy"
 
 
 @smart_ignore
-def pod_bay_doors(jenni, input):
-    jenni.say("I'm sorry Dave, I'm afraid I can't do that")
-pod_bay_doors.rule = "^!?open the pod bay doors(, $nickname)?"
+def pod_bay_doors(jenni, msg):
+    jenni.say("I'm sorry {}, I'm afraid I can't do that".format(msg.nick))
+pod_bay_doors.rule = "^open the pod bay doors(, $nickname)?"
 pod_bay_doors.priority = 'medium'
+
+
+@smart_ignore
+def therethere(jenni, input):
+    msg = u"(￣▽￣)ノ(╥﹏╥) there, there"
+    if input.groups()[0]:
+        msg += input.groups()[0]
+    jenni.say(msg)
+therethere.rule = "^!therethere(.*)"
+
+
+@smart_ignore
+def horse_to_duck(jenni, msg):
+    htd = 0.00035092493
+    horses = float(msg.groups()[0])
+    ducks = horses / htd
+    if ducks != 1:
+        ducks = "{} ducks".format(ducks)
+    else:
+        ducks = "1 duck"
+    jenni.reply("It takes {} to produce {} horsepower".format(ducks, horses))
+horse_to_duck.rule = r'^!horsetoduck ([0-9\.]+)'
+
+
+@smart_ignore
+def duck_to_horse(jenni, msg):
+    htd = 2849.61230882
+    ducks = float(msg.groups()[0])
+    horses = ducks / htd
+    if horses != 1:
+        horses = "{} horses".format(horses)
+    else:
+        horses = "1 horse"
+    jenni.reply("It takes {} to produce {} duckpower".format(horses, ducks))
+duck_to_horse.rule = r'^!ducktohorse ([0-9\.]+)'
+
+
+@smart_ignore
+def themoreyouknow(jenni, input):
+    from modules.brittbot.helpers import (colors, colorize, colorize_msg)
+    themoreyouknow1 = u"The More You Know"
+    themoreyouknow2 = u"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈"
+    star = u"★"
+    jenni.say(colorize(themoreyouknow1, fg=colors['blue']))
+    jenni.say(colorize_msg(themoreyouknow2) + colorize(star, fg=colors['yellow']) )
+themoreyouknow.rule = "^!themoreyouknow"
 
 
 @smart_ignore
@@ -53,6 +112,15 @@ def party(jenni, input):
     jenni.say(action("dances :D\-<  :D|-<  :D/-<  :D\-<  :D|-< :D/-<"))
 party.rule = "^!party"
 party.priority = 'medium'
+
+
+@smart_ignore
+def define_Word(jenni, msg):
+    from textblob import TextBlob
+    words = msg.groups()[0]
+    words = TextBlob(words).words
+    jenni.reply(words[0].define()[0])
+define_Word.rule = "^!define (.*)"
 
 
 @smart_ignore
