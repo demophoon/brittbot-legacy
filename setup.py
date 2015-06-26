fa9de2d9466108b9cc001e0958c3807c35dfc88f
Britt Gresham <brittcgresham@gmail.com>
2015-06-26 06:36:53 +0000

    Add setup.py file for easy install

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="Brittbot",
+    version="1.0.0",
+    author="Britt Gresham",
+    author_email="brittbot@brittg.com",
+    description=("My IRC Bot"),
+    license="MIT",
+    install_requires=[
+        "pillow",
+        "textblob",
+        "python-dateutil",
+        "beautifulsoup",
+        "pylast",
+        "lxml",
+    ],
+)
