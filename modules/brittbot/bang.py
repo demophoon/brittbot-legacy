e0095dbbf83859b3aeec6af66019b0beab79ab3d
ccockerhamkc <ccockerhamkc@gmail.com>
2015-06-30 23:19:09 -0500

    add a blank line to keep linters happy

--- modules/brittbot/bang.py
+++ modules/brittbot/bang.py
@@ -29,6 +29,7 @@ def rimshot(jenni, input):
 rimshot.rule = "^(!|\x01ACTION )rimshot"
 rimshot.priority = 'medium'
 
+
 @smart_ignore
 def fourofourd(jenni, input):
     jenni.say("http://www.homestarrunner.com/404error.html")
