235749061ea54b8770097ef0a305473fd822679b
Britt Gresham <brittcgresham@gmail.com>
2015-07-07 14:27:06 +0000

    Add helper methods

--- modules/brittbot/pil/enhance.py
+++ modules/brittbot/pil/enhance.py
@@ -9,15 +9,27 @@ import urllib
 from PIL import (
     Image,
     ImageEnhance,
+    ImageOps,
 )
 
 imagepath = '/var/www/htdocs/brittbot/'
 
 
-def enhance(url):
+def image_from_url(url):
     path = io.BytesIO(urllib.urlopen(url).read())
     img = Image.open(path)
     img = img.convert('RGBA')
+    return img
+
+
+def save_to_url(img):
+    imagefile = "%s.jpg" % str(uuid.uuid4()).replace('-', '')[0:8]
+    img.save(imagepath + imagefile, 'jpeg', quality=1)
+    return imagefile
+
+
+def enhance(url):
+    img = image_from_url(url)
     enhancements = [
         ImageEnhance.Color,
         ImageEnhance.Contrast,
@@ -26,7 +38,5 @@ def enhance(url):
     ]
     for _ in range(random.randint(2,6)):
         img = random.choice(enhancements)(img).enhance(random.random() * 3)
+    return save_to_url(img)
 
-    imagefile = "%s.jpg" % str(uuid.uuid4()).replace('-', '')[0:8]
-    img.save(imagepath + imagefile, 'jpeg', quality=1)
-    return imagefile
