25bc223c73fdd26741b02e390bcad24380ad2d77
Britt Gresham <brittcgresham@gmail.com>
2015-08-05 22:38:42 +0000

    Add jargon

--- modules/brittbot/test.py
+++ modules/brittbot/test.py
@@ -111,6 +111,92 @@ mnightwho.rule = r"(?i).*(m\.? night).*"
 
 
 @smart_ignore
+def tech_jargon(jenni, msg):
+    jargon = {
+        "abbreviations": [
+            "TCP", "HTTP", "SDD", "RAM", "GB", "CSS", "EMC", "GND", "VCC",
+            "SSL", "AGP", "SQL", "FTP", "PCI", "AI", "ADP", "RSS", "XML", "EXE",
+            "COM", "HDD", "THX", "SMTP", "SMS", "USB", "PNG", "PGP", "GPG", "HTTPS",
+            "GIT", "TFS", "SVN", "RBAC", "LDAP", "AD", "HTML", "IP", "IRC", "NIC",
+            "HTMLS", "PnP", "AJAX", "JSON", "HOCON", "ZIP", "TAR", "SATA3", "SATA2",
+            "PATA", "VLC", "GSM", "CDMA", "GEOIP",
+        ],
+        'adjectives': [
+            "auxiliary", "primary", "back-end", "digital", "enterprise",
+            "open-source", "virtual", "cross-platform", "redundant", "online",
+            "haptic", "multi-byte", "bluetooth", "wireless", "1080p", "neural",
+            "optical", "solid state", "mobile", "multi-threaded", "high fidelity",
+            "haskell", "python", "postgresql", "MySql", "java", "javascript",
+            "node.js", "github",
+        ],
+        'nouns': [
+            "driver", "protocol", "bandwidth", "panel",
+            "microchip", "program", "port", "card", "array", "interface", "system",
+            "sensor", "firewall", "hard drive", "pixel", "alarm", "feed", "monitor",
+            "application", "transmitter", "bus", "circuit", "capacitor", "matrix",
+            "mainframe", "keyboard", "input device", "sensor", "transistor", "ram disk",
+            "system clock", "tree", "byte", "bytecode", "firewire", "link", "cache",
+            "dot matrix", "cache matrix", "variable",
+        ],
+        'verbs': [
+            "back up", "bypass", "hack", "override", "compress",
+            "copy", "navigate", "index", "connect", "generate", "quantify",
+            "calculate", "synthesize", "input", "transmit", "program", "reboot",
+            "parse", "map", "degauss", "decouple", "download", "program", "parse",
+            "upload", "couple", "partition", "obfuscate", "recurse", "invert",
+            "increase", "decrease", "fluctuate", "jump", "kick", "restart",
+            "compile", "serialize", "deserialize", "encode", "reencode", "decode",
+            "decompile",
+        ],
+        'ingverbs': [
+            "backing up", "bypassing", "hacking",
+            "overriding", "compressing", "copying", "navigating", "indexing",
+            "connecting", "generating", "quantifying", "calculating",
+            "synthesizing", "transmitting", "programming", "parsing", "mapping",
+            "degaussing", "downloading", "uploading", "decoupling", "coupling",
+        ]
+    }
+
+    constructs = [{
+        'types': ["verb", "noun", "abbreviation", "noun", "adjective", "abbreviation", "noun"],
+        'structure': "If we {0} the {1}, we can get to the {2} {3} through the {4} {5} {6}!"
+    }, {
+        'types': ["verb", "adjective", "abbreviation", "noun"],
+        'structure': "We need to {0} the {1} {2} {3}!"
+    }, {
+        'types': ["verb", "abbreviation", "noun", "verb", "adjective", "noun"],
+        'structure': "Try to {0} the {1} {2}, maybe it will {3} the {4} {5}!"
+    }, {
+        'types': ["verb", "noun", "ingverb", "adjective", "abbreviation", "noun"],
+        'structure': "You can't {0} the {1} without {2} the {3} {4} {5}!"
+    }, {
+        'types': ["adjective", "abbreviation", "noun", "verb", "adjective", "noun"],
+        'structure': "Use the {0} {1} {2}, then you can {3} the {4} {5}!"
+    }, {
+        'types': ["abbreviation", "noun", "verb", "adjective", "noun", "verb", "abbreviation", "noun"],
+        'structure': "The {0} {1} is down, {2} the {3} {4} so we can {5} the {6} {7}!"
+    }, {
+        'types': ["ingverb", "noun", "verb", "adjective", "abbreviation", "noun"],
+        'structure': "{0} the {1} won't do anything, we need to {2} the {3} {4} {5}!"
+    }, {
+        'types': ["verb", "adjective", "abbreviation", "noun", "verb", "abbreviation", "noun"],
+        'structure': "I'll {0} the {1} {2} {3}, that should {4} the {5} {6}!"
+    }]
+
+    get_words = lambda t: jargon[t + 's']
+    get_word = lambda t: random.choice(get_words(t))
+
+    sentence_format = random.choice(constructs)
+    types = sentence_format['types']
+    words = []
+    for t in types:
+        words.append(get_word(t))
+    reply = sentence_format['structure'].format(*words)
+    jenni.say(reply)
+tech_jargon.rule = r'(?i)^(?:!jargon|$nickname\S? what do you (?:think|know)\??)'
+
+
+@smart_ignore
 def arewefriends(jenni, msg):
     if msg.friend:
         reply = "Yes"
